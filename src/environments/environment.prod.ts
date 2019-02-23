a834c2a47b97663e84c9ae53462c7c4e43243ea5
Sujith Menon <sujithsurendranath08@gmail.com>
2019-02-23 12:31:43 +0530

    Removed firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,7 @@
 export const environment = {
   production: true,
   firebase:{
-    apiKey: "AIzaSyD-0djhjgq2QAWtyfKHmHMJrm3fi9oJRNg",
-    authDomain: "newspaperinternship.firebaseapp.com",
-    databaseURL: "https://newspaperinternship.firebaseio.com",
-    projectId: "newspaperinternship",
-    storageBucket: "newspaperinternship.appspot.com",
-    messagingSenderId: "838597022399"
+    // FIREBASE CONFIG
 
   }
 };
